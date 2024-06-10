5b3a55869733c5e5425ef9a52be9f1c8071217da
amineaffif <affif.amine@live.com>
2024-06-11 00:53:13 +0200

    file upload form UI

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,7 +1,38 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
 import { Upload } from "lucide-react";
+import { FormValues } from "types/form";
+
+const validationSchema = yup.object().shape({
+  file: yup
+    .mixed()
+    .required("A file is required")
+    .test(
+      "fileSize",
+      "The file is too large",
+      (value) => value && (value as File).size <= 1024 * 1024 * 5 // 5MB
+    )
+    .test(
+      "fileType",
+      "Unsupported file format",
+      (value) =>
+        value && ["image/jpeg", "image/png"].includes((value as File).type)
+    ),
+});
 
 export function HeroSection() {
+  const [file, setFile] = useState<File | null>(null);
+  const [imageURL, setImageURL] = useState<string | null>(null);
+
+  const handleSubmit = (values: FormValues) => {
+    console.log(values.file);
+    // Handle file upload to API here
+  };
+
   return (
     <section className="w-full h-screen border-y flex justify-center items-center flex-nowrap">
       <div className="px-4 md:px-6 space-y-10 xl:space-y-16">
@@ -23,7 +54,7 @@ export function HeroSection() {
                 Essai Gratuit
               </Link>
               <Link
-                className="inline-flex h-9 items-center justify-center rounded-md border main-color-border bg-white px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:main-color-text focus-visible:outline-none main-color-focus disabled:pointer-events-none disabled:opacity-50 main-color-dark-border"
+                className="inline-flex h-9 items-center justify-center rounded-md border main-color-border main-color-border-hover bg-white px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:main-color-text focus-visible:outline-none main-color-focus disabled:pointer-events-none disabled:opacity-50 main-color-dark-border"
                 href="#"
               >
                 En savoir plus
@@ -34,17 +65,79 @@ export function HeroSection() {
             <div className="h-full container px-0 md:px-6 py-5 md:py-0">
               <div className="h-full mx-auto max-w-3xl">
                 <div className="h-full relative aspect-[4/3] md:aspect-auto overflow-hidden rounded-2xl bg-gray-100 transition-all duration-300 hover:scale-[1.02] hover:shadow-lg dark:bg-gray-800">
-                  <div className="absolute inset-0 flex items-center justify-center h-auto md:h-full">
-                    <div className="flex flex-col items-center space-y-4">
-                      <Upload />
-                      <h3 className="text-2xl font-bold text-gray-700 dark:text-gray-200">
-                        Glisser votre fichier ici
-                      </h3>
-                      <p className="text-gray-500 dark:text-gray-400">
-                        Ou clic pour pour upload
-                      </p>
-                    </div>
-                  </div>
+                  <Formik
+                    initialValues={{ file: null }}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                  >
+                    {({ setFieldValue }) => (
+                      <Form className="h-full flex flex-col items-center space-y-4">
+                        <div className="absolute inset-0">
+                          {imageURL && (
+                            <div
+                              className="absolute inset-0"
+                              style={{
+                                backgroundImage: `url(${imageURL})`,
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
+                                filter: "blur(8px)",
+                                opacity: 0.1,
+                              }}
+                            />
+                          )}
+                        </div>
+                        <div className="absolute inset-0 flex items-center justify-center h-auto md:h-full z-10 py-38 md:p-0">
+                          <div className="flex flex-col items-center space-y-4">
+                            <Upload />
+                            <h3 className="text-2xl font-bold text-gray-700 dark:text-gray-200">
+                              Glisser votre fichier ici
+                            </h3>
+                            <p className="text-gray-500 dark:text-gray-400">
+                              Ou clic pour pour upload
+                            </p>
+                            <input
+                              id="file"
+                              name="file"
+                              type="file"
+                              className="hidden"
+                              onChange={(event) => {
+                                const file =
+                                  event.currentTarget.files?.[0] || null;
+                                setFieldValue("file", file);
+                                setFile(file);
+                                if (file) {
+                                  const url = URL.createObjectURL(file);
+                                  setImageURL(url);
+                                }
+                              }}
+                            />
+                            <label
+                              htmlFor="file"
+                              className="cursor-pointer inline-flex h-9 items-center justify-center rounded-md border main-color-border main-color-border-hover bg-white px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:main-color-text focus-visible:outline-none main-color-focus disabled:pointer-events-none disabled:opacity-50 main-color-dark-border"
+                            >
+                              Choisir un fichier
+                            </label>
+                            <ErrorMessage
+                              name="file"
+                              component="div"
+                              className="text-red-500 text-sm"
+                            />
+                            {file && (
+                              <div className="text-gray-700 dark:text-gray-200 px-10 text-ellipsis truncate w-9/12">
+                                {file.name}
+                              </div>
+                            )}
+                            <button
+                              type="submit"
+                              className=" z-10 inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm main-color main-color-hover"
+                            >
+                              Envoyer
+                            </button>
+                          </div>
+                        </div>
+                      </Form>
+                    )}
+                  </Formik>
                   <img
                     src="/placeholder.svg"
                     alt="Drop zone"
