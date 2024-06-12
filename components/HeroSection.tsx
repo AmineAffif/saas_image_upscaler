f4223016bc8dd31a12e51d13d7036de3073857e4
Nojaas <jason.leroy@hetic.net>
2024-06-12 21:49:25 +0200

    add secondary color & remove component

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -38,7 +38,7 @@ export function HeroSection() {
       <div className="px-4 md:px-6 space-y-10 xl:space-y-16">
         <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2">
           <div className="flex flex-col justify-start mt-0 lg:mt-8">
-            <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem] main-color-text">
+            <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem] secondary-color-text">
               Liberez le plein potentiel de vos images grâce à l'IA
             </h1>
             <p className="mx-auto max-w-[700px] text-gray-500 md:text-l mt-4 dark:text-gray-400">
