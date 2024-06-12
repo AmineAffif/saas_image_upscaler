8c9910caaf69fcdce661f3922d70c7efa0069cf3
amineaffif <affif.amine@live.com>
2024-06-12 15:50:25 +0200

    image comparison slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { HeroSection } from "components/HeroSection";
 import { FeaturesSection } from "components/FeaturesSection";
 import { PricingSection } from "components/PricingSection";
 import { ReviewsSection } from "components/ReviewsSection";
+import ComparisonSection from "components/ComparisonSection";
 
 import { Header } from "components/Header";
 import Footer from "components/Footer";
@@ -12,6 +15,7 @@ export default function LandingPage() {
       <Header />
       <main className="flex-1">
         <HeroSection />
+        <ComparisonSection />
         <FeaturesSection />
         <ReviewsSection />
         <PricingSection />
---
+++ components/ComparisonSection.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+import { ImgComparisonSlider } from "@img-comparison-slider/react";
+
+export default function ComparisonSection() {
+  return (
+    <div className="h-screen w-full px-4 lg:px-24 pt-0 pb-20 flex flex-col lg:flex-row justify-center items-center">
+      <ImgComparisonSlider>
+        <div slot="first">
+          <Image
+            src="/landscape-pixelized.webp"
+            alt="Before"
+            width={980}
+            height={551}
+          />
+        </div>
+        <div slot="second">
+          <Image src="/landscape.webp" alt="After" width={980} height={551} />
+        </div>
+      </ImgComparisonSlider>
+      <div className="flex flex-col items-start mt-10 lg:mt-0 ml-0 lg:ml-10">
+        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+          Rattrapez
+        </h2>
+        <span className="text-sm font-extralight tracking-normal">
+          (presque)
+        </span>{" "}
+        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+          Tous les défauts
+        </h2>
+      </div>
+    </div>
+  );
+}
