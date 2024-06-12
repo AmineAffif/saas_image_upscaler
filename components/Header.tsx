982d9126ddd5c654f0669cb94104594cfd9de3e3
amineaffif <affif.amine@live.com>
2024-06-12 16:32:23 +0200

    add logo on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import { MountainIcon } from "./Icons/MountainIcon";
 import { MenuIcon } from "./Icons/MenuIcon";
 import { MobileMenu } from "./MobileMenu";
 import { XIcon } from "./Icons/XIcon";
+import Image from "next/image";
 
 export function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,9 +18,10 @@ export function Header() {
   return (
     <header className="px-4 md:px-20 lg:px-28 h-14 flex items-center justify-between absolute top-0 left-0 w-full">
       <Link className="flex items-center justify-center" href="#">
-        <MountainIcon className="h-6 w-6" />
+        {/* <MountainIcon className="h-6 w-6" /> */}
+        <Image src="/logo-scaly-min.jpg" alt="Before" width={35} height={35} />
         <span className="ml-2 text-black">Scaly</span>
-        <span className="sr-only">Image Upscaler</span>
+        <span className="sr-only">Scaly</span>
       </Link>
       <nav className="ml-auto hidden lg:flex gap-4 sm:gap-6 items-center">
         <Link
