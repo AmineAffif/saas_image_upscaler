6b19c0e0512b4d545034de7634209aa4464bf272
Nojaas <jason.leroy@hetic.net>
2024-06-14 11:56:47 +0200

    comment aurora bg

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,24 @@ import ExampleSection from "components/ExampleSection";
 import { Header } from "components/Header";
 import Footer from "components/Footer";
 import { FaqSection } from "components/FaqSection";
+// import { AuroraBackground } from "@/components/ui/aurora-background";
+// import { motion } from "framer-motion";
 
 export default function LandingPage() {
   return (
     <div className="flex flex-col min-h-[100dvh]">
       <Header />
+      {/* <AuroraBackground>
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="relative flex flex-col gap-4 items-center justify-center px-4"
+        > */}
       <main className="flex-1">
         <HeroSection />
         <ComparisonSection />
@@ -25,6 +38,8 @@ export default function LandingPage() {
         <FaqSection />
         <Footer />
       </main>
+      {/* </motion.div>
+      </AuroraBackground> */}
     </div>
   );
 }
