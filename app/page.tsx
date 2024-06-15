364f06e8c1535f05206890e98d6edefd825b059e
Nojaas <jason.leroy@hetic.net>
2024-06-15 12:09:22 +0200

    remove aurora bg

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ import { FeaturesSection } from "components/FeaturesSection";
 import { HeroSection } from "components/HeroSection";
 import { PricingSection } from "components/PricingSection";
 import { ReviewsSection } from "components/ReviewsSection";
-
-import { AuroraBackground } from "@/components/ui/aurora-background";
 import { FaqSection } from "components/FaqSection";
 import Footer from "components/Footer";
 import { Header } from "components/Header";
@@ -18,30 +16,28 @@ export default function LandingPage() {
   return (
     <div className="flex flex-col min-h-[100dvh]">
       <Header />
-      <AuroraBackground>
-        <motion.div
-          initial={{ opacity: 0.0, y: 40 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="flex flex-col min-h-[100dvh]"
-        >
-          <main className="flex-1">
-            <HeroSection />
-            <ComparisonSection />
-            <FeaturesSection />
-            <ReviewsSection />
-            <UserProofSection />
-            <PricingSection />
-            <ExampleSection />
-            <FaqSection />
-            <Footer />
-          </main>
-        </motion.div>
-      </AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.4,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="flex flex-col min-h-[100dvh]"
+      >
+        <main className="flex-1">
+          <HeroSection />
+          <ComparisonSection />
+          <FeaturesSection />
+          <ReviewsSection />
+          <UserProofSection />
+          <PricingSection />
+          <ExampleSection />
+          <FaqSection />
+          <Footer />
+        </main>
+      </motion.div>
     </div>
   );
 }
